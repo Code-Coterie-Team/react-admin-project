dcd8203d431113fb1dae79cf2dcc81f18af9526f
faeze <faezeh.am4212@gmail.com>
2024-12-25 15:21:04 +0330

    installReChartAndAddChartOverview

---
+++ src/components/ChartSalesOverview.tsx
@@ -0,0 +1,58 @@
+import {
+  LineChart,
+  Line,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
+
+function ChartSalesOverview() {
+  const data = [
+    { name: "Jul", sales: 4200 },
+    { name: "Aug", sales: 3800 },
+    { name: "Sep", sales: 5100 },
+    { name: "Oct", sales: 4600 },
+    { name: "Nov", sales: 5400 },
+    { name: "Dec", sales: 7200 },
+    { name: "Jan", sales: 6100 },
+    { name: "Feb", sales: 5900 },
+    { name: "Mar", sales: 6800 },
+    { name: "Apr", sales: 6300 },
+    { name: "May", sales: 7100 },
+    { name: "Jun", sales: 7500 },
+  ];
+
+  return (
+    <div className="chart-container h-80">
+      <ResponsiveContainer
+        width="100%"
+        height="100%"
+      >
+        <LineChart
+          data={data}
+          margin={{ top: 5, right: 20, bottom: 5, left: 0 }}
+        >
+          <Line
+            type="monotone"
+            dataKey="sales"
+            stroke="#8884d8" // رنگ خط
+            strokeWidth={3} // پهنای خط
+            activeDot={{ r: 8, fill: "#6366f1" }}
+            dot={{ r: 8, fill: "#6366f1" }} // دایره اصلی
+          />
+          <CartesianGrid
+            stroke="#e5e5e5"
+            strokeDasharray="5 5"
+          />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+        </LineChart>
+      </ResponsiveContainer>
+    </div>
+  );
+}
+
+export default ChartSalesOverview;
--- src/components/Overview.tsx
+++ src/components/Overview.tsx
@@ -1,9 +1,10 @@
-// import React from 'react'
-// import HeaderPanel from "./HeaderPanel"
 import Panel from "./Panel"
 import { Flash, Users, ShoppingBag, BarChart01 } from "../icons"
-function Overview() {
+import ChartSalesOverview from "./chartSalesOverview"
 
+
+function Overview() {
+  
     return (
         <div className='overviewPage main py-6 px-8 h-screen'>
             <div className='grid grid-cols-4 gap-3 pb-8'>
@@ -12,9 +13,12 @@ function Overview() {
                 <Panel icon={<ShoppingBag className="text-pink-500" width="1.5rem" height="1.rem"/>} title="Total Products" amount={567} type="" />
                 <Panel icon={<BarChart01 className="text-green-600" width="1.5rem" height="1.5rem"/>} title="Conversion Rate" amount={12.5} type="persent" />
             </div>
-            <div className="chartOverview grid grid-cols-2 gap-4">
+            <div className="chartOverview grid grid-cols-1 lg:grid-cols-2 gap-4">
                 <div className='bg-menuBody p-5 rounded-lg border-bmenuBody border-[1px] border-solid'>
-                    chartOverview01
+                    <h2 className="text-lg font-medium pb-4 text-gray-100">Sales Overview</h2>
+                  
+                    <ChartSalesOverview/>
+                   
                 </div>
                 <div className="bg-menuBody p-5 rounded-lg border-bmenuBody border-[1px] border-solid ">
                     chartOverview02
