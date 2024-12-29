4924b525f6066b7df1ca03e7dfc75fd526d63721
faeze <faezeh.am4212@gmail.com>
2024-12-29 22:35:46 +0330

    retryInApi

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      retry: 1,
+      retry: 5,
     },
   },
 });
