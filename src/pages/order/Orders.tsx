d97d622ab5b561ba536c80003d885d9cde4a4b0e
faeze <faezeh.am4212@gmail.com>
2025-02-14 01:22:46 +0330

    addChartFrameworkAddPanelAndMapONTable

--- src/pages/order/Orders.tsx
+++ src/pages/order/Orders.tsx
@@ -6,77 +6,107 @@ import {
   Eyse,
   Search02,
 } from "../../icons";
-import ChartDailyOrders from "./components/ChartDailyOrders";
-import ChartOrderStatusDistribution from "./components/ChartOrderStatusDistribution";
 import Panel from "../../components/Panel";
+import ChartOrderStatusDistribution from "./components/ChartOrderStatusDistribution";
+import ChartDailyOrders from "./components/ChartDailyOrders";
+import ChartFramework from "../../components/ChartFramework";
 
 function Orders() {
+ const chartOrder = [
+    {
+      title: "Daily Orders",
+      nameChart: <ChartDailyOrders />,
+    },
+    {
+      title: " Order Status Distribution",
+      nameChart: <ChartOrderStatusDistribution />,
+    },
+  ]; 
+
+  const ArrayPanelOrders = [
+    {
+      icon: (
+        <ShoppingBag
+          className="text-indigo-600"
+          width="1.5rem"
+          height="1.5rem"
+        />
+      ),
+      title: "Total Orders",
+      amount: 1234,
+      type: "",
+    },
+    {
+      icon: (
+        <Alarm
+          className="text-yellow-500"
+          width="1.5rem"
+          height="1.5rem"
+        />
+      ),
+      title: "Pending Orders",
+      amount: 56,
+      type: "",
+    },
+    {
+      icon: (
+        <CheckMarkCircle
+          className="text-green-600"
+          width="1.5rem"
+          height="1.5rem"
+        />
+      ),
+      title: "Completed Orders",
+      amount: 1178,
+      type: "",
+    },
+    {
+      icon: (
+        <Dollar
+          className="text-red-500"
+          width="1.5rem"
+          height="1.5rem"
+        />
+      ),
+      title: "Total Revenue",
+      amount: 98765,
+      type: "asset",
+    },
+  ];
+  const dataTableOrder = [
+    {
+      orderId: "ORD001",
+      customer: "John Doe",
+      total: "$235.40",
+      status: "Delivered",
+      date: "2023-07-01",
+    },
+    {
+      orderId: "ORD002",
+      customer: "Jane Smith",
+      total: "$150.00",
+      status: "Processing",
+      date: "2023-07-02",
+    },
+    {
+      orderId: "ORD003",
+      customer: "Alice Johnson",
+      total: "$300.50",
+      status: "Shipped",
+      date: "2023-07-03",
+    },
+    {
+      orderId: "ORD004",
+      customer: "Bob Brown",
+      total: "$120.00",
+      status: "Pending",
+      date: "2023-07-04",
+    },
+  ];
   return (
     <div className="productPage main py-6 px-8">
-      <div className="grid grid-cols-4 gap-3 pb-8">
-        <Panel
-          icon={
-            <ShoppingBag
-              className="text-indigo-600"
-              width="1.5rem"
-              height="1.5rem"
-            />
-          }
-          title="Total Orders"
-          amount={1234}
-          type=""
-        />
-        <Panel
-          icon={
-            <Alarm
-              className="text-yellow-500"
-              width="1.5rem"
-              height="1.5rem"
-            />
-          }
-          title="Pending Orders"
-          amount={56}
-          type=""
-        />
-        <Panel
-          icon={
-            <CheckMarkCircle
-              className="text-green-600"
-              width="1.5rem"
-              height="1.5rem"
-            />
-          }
-          title="Completed Orders"
-          amount={1178}
-          type=""
-        />
-        <Panel
-          icon={
-            <Dollar
-              className="text-red-500"
-              width="1.5rem"
-              height="1.5rem"
-            />
-          }
-          title="Total Revenue"
-          amount={98765}
-          type="asset"
-        />
-      </div>
-      <div className="chartProducts mb-8 grid grid-cols-2 gap-4">
-        <div className="bg-menuBody p-5 rounded-lg border-bmenuBody border border-solid ">
-          <h2 className="text-lg font-medium pb-4 text-gray-100">
-            Daily Orders
-          </h2>
-          <ChartDailyOrders />
-        </div>
-        <div className="bg-menuBody p-5 rounded-lg border-bmenuBody border border-solid ">
-          <h2 className="text-lg font-medium pb-4 text-gray-100">
-            Order Status Distribution
-          </h2>
-          <ChartOrderStatusDistribution />
-        </div>
-      </div>
+      <Panel panels={ArrayPanelOrders} />
+      <ChartFramework charts={chartOrder} />
       <div className="productList  bg-menuBody p-5 rounded-lg border-bmenuBody border border-solid ">
         <div className="flex pb-7 justify-between items-center">
           <h2 className="text-xl text-gray-100 font-semibold">Order List</h2>
@@ -120,182 +150,57 @@ function Orders() {
               </tr>
             </thead>
             <tbody>
-              <tr className="tableBorderB">
-                <td className="px-6 py-4 text-sm font-medium">ORD001</td>
-                <td className="px-6 py-4 text-sm font-medium">John Doe</td>
-                <td className="px-6 py-4 text-sm font-medium"> $235.40</td>
-                <td className="px-6 py-4">
-                  <span className="bg-green-100 text-green-800 font-semibold text-xs rounded-full px-2">
-                    Delivered
-                  </span>
-                </td>
-                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-300">
-                  2023-07-01
-                </td>
-                <td className="px-6 py-4">
-                  <button>
-                    <Eyse
-                      className="text-indigo-400"
-                      width="1.25rem"
-                      height="1.25rem"
-                    />
-                  </button>
-                </td>
-              </tr>
-              <tr className="tableBorderB">
-                <td className="px-6 py-4 text-sm font-medium">ORD001</td>
-                <td className="px-6 py-4 text-sm font-medium">John Doe</td>
-                <td className="px-6 py-4 text-sm font-medium"> $235.40</td>
-                <td className="px-6 py-4">
-                  <span className="bg-yellow-100 text-yellow-800 font-semibold text-xs rounded-full px-2">
-                    Processing
-                  </span>
-                </td>
-                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-300">
-                  2023-07-01
-                </td>
-                <td className="px-6 py-4">
-                  <button>
-                    <Eyse
-                      className="text-indigo-400"
-                      width="1.25rem"
-                      height="1.25rem"
-                    />
-                  </button>
-                </td>
-              </tr>
-              <tr className="tableBorderB">
-                <td className="px-6 py-4 text-sm font-medium">ORD001</td>
-                <td className="px-6 py-4 text-sm font-medium">John Doe</td>
-                <td className="px-6 py-4 text-sm font-medium"> $235.40</td>
-                <td className="px-6 py-4">
-                  <span className="bg-blue-100 text-blue-800 font-semibold text-xs rounded-full px-2">
-                    Shipped
-                  </span>
-                </td>
-                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-300">
-                  2023-07-01
-                </td>
-                <td className="px-6 py-4">
-                  <button>
-                    <Eyse
-                      className="text-indigo-400"
-                      width="1.25rem"
-                      height="1.25rem"
-                    />
-                  </button>
-                </td>
-              </tr>
-              <tr className="tableBorderB">
-                <td className="px-6 py-4 text-sm font-medium">ORD001</td>
-                <td className="px-6 py-4 text-sm font-medium">John Doe</td>
-                <td className="px-6 py-4 text-sm font-medium"> $235.40</td>
-                <td className="px-6 py-4">
-                  <span className="bg-red-100 text-red-800 font-semibold text-xs rounded-full px-2">
-                    Pending
-                  </span>
-                </td>
-                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-300">
-                  2023-07-01
-                </td>
-                <td className="px-6 py-4">
-                  <button>
-                    <Eyse
-                      className="text-indigo-400"
-                      width="1.25rem"
-                      height="1.25rem"
-                    />
-                  </button>
-                </td>
-              </tr>
-              <tr className="tableBorderB">
-                <td className="px-6 py-4 text-sm font-medium">ORD001</td>
-                <td className="px-6 py-4 text-sm font-medium">John Doe</td>
-                <td className="px-6 py-4 text-sm font-medium"> $235.40</td>
-                <td className="px-6 py-4">
-                  <span className="bg-green-100 text-green-800 font-semibold text-xs rounded-full px-2">
-                    Delivered
-                  </span>
-                </td>
-                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-300">
-                  2023-07-01
-                </td>
-                <td className="px-6 py-4">
-                  <button>
-                    <Eyse
-                      className="text-indigo-400"
-                      width="1.25rem"
-                      height="1.25rem"
-                    />
-                  </button>
-                </td>
-              </tr>
-              <tr className="tableBorderB">
-                <td className="px-6 py-4 text-sm font-medium">ORD001</td>
-                <td className="px-6 py-4 text-sm font-medium">John Doe</td>
-                <td className="px-6 py-4 text-sm font-medium"> $235.40</td>
-                <td className="px-6 py-4">
-                  <span className="bg-green-100 text-green-800 font-semibold text-xs rounded-full px-2">
-                    Delivered
-                  </span>
-                </td>
-                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-300">
-                  2023-07-01
-                </td>
-                <td className="px-6 py-4">
-                  <button>
-                    <Eyse
-                      className="text-indigo-400"
-                      width="1.25rem"
-                      height="1.25rem"
-                    />
-                  </button>
-                </td>
-              </tr>
-              <tr className="tableBorderB">
-                <td className="px-6 py-4 text-sm font-medium">ORD001</td>
-                <td className="px-6 py-4 text-sm font-medium">John Doe</td>
-                <td className="px-6 py-4 text-sm font-medium"> $235.40</td>
-                <td className="px-6 py-4">
-                  <span className="bg-green-100 text-green-800 font-semibold text-xs rounded-full px-2">
-                    Delivered
-                  </span>
-                </td>
-                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-300">
-                  2023-07-01
-                </td>
-                <td className="px-6 py-4">
-                  <button>
-                    <Eyse
-                      className="text-indigo-400"
-                      width="1.25rem"
-                      height="1.25rem"
-                    />
-                  </button>
-                </td>
-              </tr>
-              <tr>
-                <td className="px-6 py-4 text-sm font-medium">ORD001</td>
-                <td className="px-6 py-4 text-sm font-medium">John Doe</td>
-                <td className="px-6 py-4 text-sm font-medium"> $235.40</td>
-                <td className="px-6 py-4">
-                  <span className="bg-green-100 text-green-800 font-semibold text-xs rounded-full px-2">
-                    Delivered
-                  </span>
-                </td>
-                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-300">
-                  2023-07-01
-                </td>
-                <td className="px-6 py-4">
-                  <button>
-                    <Eyse
-                      className="text-indigo-400"
-                      width="1.25rem"
-                      height="1.25rem"
-                    />
-                  </button>
-                </td>
-              </tr>
+              {dataTableOrder.map((order, index) => (
+                <tr
+                  key={index}
+                  className="tableBorderB"
+                >
+                  <td className="px-6 py-4 text-sm font-medium">
+                    {order.orderId}
+                  </td>
+                  <td className="px-6 py-4 text-sm font-medium">
+                    {order.customer}
+                  </td>
+                  <td className="px-6 py-4 text-sm font-medium">
+                    {order.total}
+                  </td>
+                  <td className="px-6 py-4">
+                    <span
+                      className={`bg-${
+                        order.status === "Delivered"
+                          ? "green"
+                          : order.status === "Processing"
+                          ? "yellow"
+                          : order.status === "Shipped"
+                          ? "blue"
+                          : "red"
+                      }-100 text-${
+                        order.status === "Delivered"
+                          ? "green"
+                          : order.status === "Processing"
+                          ? "yellow"
+                          : order.status === "Shipped"
+                          ? "blue"
+                          : "red"
+                      }-800 font-semibold text-xs rounded-full px-2`}
+                    >
+                      {order.status}
+                    </span>
+                  </td>
+                  <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-300">
+                    {order.date}
+                  </td>
+                  <td className="px-6 py-4">
+                    <button>
+                      <Eyse
+                        className="text-indigo-400"
+                        width="1.25rem"
+                        height="1.25rem"
+                      />
+                    </button>
+                  </td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </div>
