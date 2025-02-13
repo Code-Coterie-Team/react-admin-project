68d3224f49707c415a713c6fda5e78bfef097e85
faeze <faezeh.am4212@gmail.com>
2025-02-14 01:21:20 +0330

    changePanelComponent

--- src/components/Panel.tsx
+++ src/components/Panel.tsx
@@ -1,17 +1,21 @@
-function Panel(props: IPanelProps) {
+function Panel({ panels }: IPanelPages) {
   return (
-    <div
-      className="totalSales bg-menuBody p-5 rounded-lg border-bmenuBody border 
-      border-solid hover:shadow-itemMenuOverview hover:-translate-y-1"
-    >
-      <div className="flex items-center font-medium text-gray-400 text-sm">
-        {props.icon}
-        <span className="ml-1">{props.title}</span>
-      </div>
-      <p className="font-bold text-3xl pt-1">
-        {props.type === "asset" ? "$" : ""}
-        {props.amount} {props.type === "persent" ? "%" : ""}
-      </p>
+    <div className="grid grid-cols-4 gap-3 pb-8">
+      {panels.map((item) => (
+        <div
+          className="totalSales bg-menuBody p-5 rounded-lg border-bmenuBody border 
+border-solid hover:shadow-itemMenuOverview hover:-translate-y-1"
+        >
+          <div className="flex items-center font-medium text-gray-400 text-sm">
+            {item.icon}
+            <span className="ml-1">{item.title}</span>
+          </div>
+          <p className="font-bold text-3xl pt-1">
+            {item.type === "asset" ? "$" : ""}
+            {item.amount} {item.type === "persent" ? "%" : ""}
+          </p>
+        </div>
+      ))}
     </div>
   );
 }
