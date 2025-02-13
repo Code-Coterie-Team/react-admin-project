106e44cbfa9188ab5794525f4fde3d11411a75c8
faeze <faezeh.am4212@gmail.com>
2025-02-13 22:58:54 +0330

    changeName

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,5 @@
 // import React from 'react'
-import Sidbar from './Sidbar'
+import Sidebar from './Sidebar'
 import Header from './Header'
 import React from 'react'
 import { useStore } from "../store";
@@ -9,9 +9,9 @@ const Layout = (props: { children: React.ReactNode }) => {
     const menuBar = useStore((state) => state.menuBar);
 
     return (
-        <div className="grid grid-cols-12 bg-gray-900 text-gray-50">
-            <Sidbar />
-            <div className={`header ${menuBar ? "col-span-11" : "col-span-10"}`}>
+        <div className="grid grid-cols-12 bg-gray-900 text-gray-50 ">
+            <Sidebar />
+            <div className={`header  ${menuBar ? " col-span-11" : " col-span-10"}`}>
                 <Header />
                 {props.children}
             </div>
