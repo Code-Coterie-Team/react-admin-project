74a0e60e52e955917212bb9e3110bb1b636105ce
faeze <faezeh.am4212@gmail.com>
2025-02-14 01:25:31 +0330

    addChartFrameworkAndAddPanel

--- src/pages/overview/Overview.tsx
+++ src/pages/overview/Overview.tsx
@@ -3,74 +3,75 @@ import { Flash, Users, ShoppingBag, BarChart01 } from "../../icons";
 import ChartSalesOverview from "./components/ChartSalesOverview";
 import ChartCategoryDistribution from "./components/ChartCategoryDistributionOverview";
 import ChartSalesByChannel from "./components/ChartSalesByChannelOverview";
+import ChartFramework from "../../components/ChartFramework";
 
 function Overview() {
+  const panelOverview = [
+    {
+      icon: (
+        <Flash
+          className="text-indigo-600"
+          width="1.5rem"
+          height="1.5rem"
+        />
+      ),
+      title: "Total Sales",
+      amount: 12345,
+      type: "asset",
+    },
+    {
+      icon: (
+        <Users
+          className="text-purple-600"
+          width="1.5rem"
+          height="1.5rem"
+        />
+      ),
+      title: "New Users",
+      amount: 1234,
+      type: "",
+    },
+    {
+      icon: (
+        <ShoppingBag
+          className="text-pink-500"
+          width="1.5rem"
+          height="1.5rem"
+        />
+      ),
+      title: "Total Products",
+      amount: 567,
+      type: "",
+    },
+    {
+      icon: (
+        <BarChart01
+          className="text-green-600"
+          width="1.5rem"
+          height="1.5rem"
+        />
+      ),
+      title: "Conversion Rate",
+      amount: 12.5,
+      type: "persent",
+    },
+  ];
+  const chartOverview = [
+    {
+      title: "Sales Overview",
+      nameChart: <ChartSalesOverview />,
+    },
+    {
+      title: " Category Distribution",
+      nameChart: <ChartCategoryDistribution />,
+    },
+  ];
   return (
     <div className="overviewPage main py-6 px-8">
-      <div className="grid grid-cols-4 gap-3 pb-8">
-        <Panel
-          icon={
-            <Flash
-              className="text-indigo-600"
-              width="1.5rem"
-              height="1.5rem"
-            />
-          }
-          title="Total Sales"
-          amount={12345}
-          type="asset"
-        />
-        <Panel
-          icon={
-            <Users
-              className="text-purple-600"
-              width="1.5rem"
-              height="1.5rem"
-            />
-          }
-          title="New Users"
-          amount={1234}
-          type=""
-        />
-        <Panel
-          icon={
-            <ShoppingBag
-              className="text-pink-500"
-              width="1.5rem"
-              height="1.5rem"
-            />
-          }
-          title="Total Products"
-          amount={567}
-          type=""
-        />
-        <Panel
-          icon={
-            <BarChart01
-              className="text-green-600"
-              width="1.5rem"
-              height="1.5rem"
-            />
-          }
-          title="Conversion Rate"
-          amount={12.5}
-          type="persent"
-        />
-      </div>
-      <div className="chartOverview grid grid-cols-1 lg:grid-cols-2 gap-4">
-        <div className="bg-menuBody p-5 rounded-lg border-bmenuBody border border-solid">
-          <h2 className="text-lg font-medium pb-4 text-gray-100">
-            Sales Overview
-          </h2>
+      <Panel panels={panelOverview} />
 
-          <ChartSalesOverview />
-        </div>
-        <div className="bg-menuBody p-5 rounded-lg border-bmenuBody border border-solid ">
-          <h2 className="text-lg font-medium pb-4 text-gray-100">
-            Category Distribution
-          </h2>
-          <ChartCategoryDistribution />
-        </div>
+      <ChartFramework charts={chartOverview} />
+      <div className="chartOverview grid grid-cols-1 lg:grid-cols-2 gap-4">
         <div className="lg:col-span-2 bg-menuBody p-5 rounded-lg border-bmenuBody border border-solid ">
           <h2 className="text-lg font-medium pb-4 text-gray-100">
             Sales by Channel
