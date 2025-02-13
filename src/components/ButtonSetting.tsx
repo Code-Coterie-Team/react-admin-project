98ebf106703765c02fa9ad14674836331028b710
faeze <faezeh.am4212@gmail.com>
2025-02-14 03:24:18 +0330

    createButtonSetting

---
+++ src/components/ButtonSetting.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { IButtonSetting } from "../types/api";
+
+function ButtonSetting(props: IButtonSetting) {
+  return (
+    <button
+      className={`py-2 px-4 rounded font-bold bg-indigo-600 hover:bg-indigo-700 text-white ${
+        props.titelButton === "Delete Account" ? "bg-red-600 hover:bg-red-700" : ""
+      }`}
+    >
+      {props.titelButton}
+    </button>
+  );
+}
+
+export default ButtonSetting;
