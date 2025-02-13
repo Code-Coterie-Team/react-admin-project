08419971161377245ced3530b4d301c36d67f722
faeze <faezeh.am4212@gmail.com>
2025-02-14 03:25:12 +0330

    createItemNOtifiication

---
+++ src/components/ItemNOtifiication.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { IListNotifications } from '../types/api'
+
+function ItemNOtifiication({itemNotification} : IListNotifications) {
+  return (
+    <div className="flex flex-col  text-base">
+        {itemNotification.map((item) => (
+          <div className="pushNotification flex justify-between items-center py-3">
+            <span className="text-gray-300 ">{item.title}</span>
+
+            {item.state}
+          </div>
+        ))}
+     
+      </div>
+  )
+}
+
+export default ItemNOtifiication
