4c1e8803e792cb2e268462e79c5a28a5b502910b
faeze <faezeh.am4212@gmail.com>
2025-02-13 23:01:12 +0330

    deleteInterfaceAndChangeName

--- src/components/Panel.tsx
+++ src/components/Panel.tsx
@@ -1,31 +1,19 @@
-import { ReactNode } from 'react'
-
-interface IPanelProps {
-  icon: ReactNode;
-  title: string;
-  amount: number;
-  type: "persent" | "aset" | "";
-
-}
-
 function Panel(props: IPanelProps) {
-
   return (
-    <div className='totalSales bg-menuBody p-5 rounded-lg border-bmenuBody border-[1px] border-solid hover:shadow-itemMenuOverview hover:translate-y-[-5px]'>
-      <div className='flex items-center font-medium text-gray-400 text-sm'>
+    <div
+      className="totalSales bg-menuBody p-5 rounded-lg border-bmenuBody border 
+      border-solid hover:shadow-itemMenuOverview hover:-translate-y-1"
+    >
+      <div className="flex items-center font-medium text-gray-400 text-sm">
         {props.icon}
-        <span className="ml-1">
-
-          {props.title}
-        </span>
+        <span className="ml-1">{props.title}</span>
       </div>
-      <p className='font-bold text-3xl pt-1'>
-
-        {(props.type === "aset" ? "$" : "") }{props.amount} {(props.type === "persent" ? "%" : "")}
-
+      <p className="font-bold text-3xl pt-1">
+        {props.type === "asset" ? "$" : ""}
+        {props.amount} {props.type === "persent" ? "%" : ""}
       </p>
     </div>
-  )
+  );
 }
 
-export default Panel
+export default Panel;
