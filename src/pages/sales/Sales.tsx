296f17757fa4c74c408ebfa43a2597dbec5327f6
faeze <faezeh.am4212@gmail.com>
2025-02-14 01:44:03 +0330

    useChartFramework

--- src/pages/sales/Sales.tsx
+++ src/pages/sales/Sales.tsx
@@ -5,61 +5,73 @@ import ChartDailySalesTrend from "./components/ChartDailySalesTrend";
 import ChartSalesByCategory from "./components/ChartSalesByCategory";
 import ChartSalesOverview from "./components/ChartSalesOverview";
 import Panel from "../../components/Panel";
+import ChartFramework from "../../components/ChartFramework";
 
 function Sales() {
+  const panelSales = [
+    {
+      icon: (
+        <Dollar
+          className="text-indigo-600"
+          width="1.5rem"
+          height="1.5rem"
+        />
+      ),
+      title: "Total Revenue",
+      amount: 1234567,
+      type: "asset",
+    },
+    {
+      icon: (
+        <Cart
+          className="text-green-600"
+          width="1.5rem"
+          height="1.5rem"
+        />
+      ),
+      title: "Avg. Order Value",
+      amount: 78.9,
+      type: "asset",
+    },
+    {
+      icon: (
+        <LineChart
+          className="text-yellow-500"
+          width="1.5rem"
+          height="1.5rem"
+        />
+      ),
+      title: "Conversion Rate",
+      amount: 3.45,
+      type: "persent",
+    },
+    {
+      icon: (
+        <Layout06
+          className="text-red-500"
+          width="1.5rem"
+          height="1.5rem"
+        />
+      ),
+      title: "Sales Growth",
+      amount: 12.3,
+      type: "persent",
+    },
+  ];
+  const chartSales = [
+    {
+      title: "Sales by Category",
+      nameChart: ( <ChartSalesByCategory />),
+    },
+    {
+      title: "Daily Sales Trend",
+      nameChart: (  <ChartDailySalesTrend />),
+    },
+  ]; 
   return (
     <div className="overviewPage main py-6 px-8">
-      <div className="grid grid-cols-4 gap-3 pb-8">
-        <Panel
-          icon={
-            <Dollar
-              className="text-indigo-600"
-              width="1.5rem"
-              height="1.5rem"
-            />
-          }
-          title="Total Revenue"
-          amount={1234567}
-          type="asset"
-        />
-        <Panel
-          icon={
-            <Cart
-              className="text-green-600"
-              width="1.5rem"
-              height="1.5rem"
-            />
-          }
-          title="Avg. Order Value"
-          amount={78.9}
-          type="asset"
-        />
-        <Panel
-          icon={
-            <LineChart
-              className="text-yellow-500"
-              width="1.5rem"
-              height="1.5rem"
-            />
-          }
-          title="Conversion Rate"
-          amount={3.45}
-          type="persent"
-        />
-        <Panel
-          icon={
-            <Layout06
-              className="text-red-500"
-              width="1.5rem"
-              height="1.5rem"
-            />
-          }
-          title="Sales Growth"
-          amount={12.3}
-          type="persent"
-        />
-      </div>
-      <div className="chartOverview grid grid-cols-2 gap-4">
+      <Panel panels={panelSales} />
+      <div className="chartOverview grid grid-cols-1 gap-4">
         <div className="lg:col-span-2 bg-menuBody p-5 rounded-lg border-bmenuBody border border-solid">
           <div className="flex items-center justify-between mb-6">
             <h2 className="text-lg font-medium pb-4 text-gray-100">
@@ -78,18 +90,7 @@ function Sales() {
           </div>
           <ChartSalesOverview />
         </div>
-        <div className="bg-menuBody p-5 rounded-lg border-bmenuBody border border-solid ">
-          <h2 className="text-lg font-medium pb-4 text-gray-100">
-            Sales by Category
-          </h2>
-          <ChartSalesByCategory />
-        </div>
-        <div className="bg-menuBody p-5 rounded-lg border-bmenuBody border border-solid ">
-          <h2 className="text-lg font-medium pb-4 text-gray-100">
-            Daily Sales Trend
-          </h2>
-          <ChartDailySalesTrend />
-        </div>
+        <ChartFramework charts={chartSales} />
       </div>
     </div>
   );
