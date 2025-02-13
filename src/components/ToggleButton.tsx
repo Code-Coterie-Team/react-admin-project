5a91ef45bb6fc3d9a40208f9c7400bb98941bdb9
faeze <faezeh.am4212@gmail.com>
2025-02-14 03:27:10 +0330

    createToggleButton

---
+++ src/components/ToggleButton.tsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+function ToggleButton({ initialState }: any) {
+  const [toggleButton, setToggleButton] = useState(initialState);
+
+  const handleToggleButton = () => {
+    setToggleButton((prevState:any) => !prevState);
+  };
+  return (
+    <button
+      className={` h-6 w-11 rounded-full inline-flex items-center 
+     ${toggleButton === false ? "bg-gray-600" : " bg-indigo-600"}
+    `}
+      onClick={() => {
+        handleToggleButton();
+      }}
+    >
+      <span
+        className={`bg-gray-100 rounded-full size-4 inline-block 
+   ${toggleButton === false ? "ml-1" : "ml-[1.5rem]"}`}
+      ></span>
+    </button>
+  );
+}
+
+export default ToggleButton;
