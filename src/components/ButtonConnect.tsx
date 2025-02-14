0fe27ef9aa00603c3ee9d887cc26fada73b7e4f7
faeze <faezeh.am4212@gmail.com>
2025-02-14 12:24:35 +0330

    createButtonConnect

---
+++ src/components/ButtonConnect.tsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import { InitialState } from "../types/api";
+
+function ButtonConnect({ initialState }: InitialState) {
+  const [connectAccounts, setConnectAccounts] = useState(initialState);
+
+  function ButtonConnectAccounts() {
+    setConnectAccounts((prevState: any) => !prevState);
+  }
+  return (
+    <button
+      onClick={() => {
+        ButtonConnectAccounts();
+      }}
+      className={` py-1 rounded focus:outline-none ${
+        connectAccounts === false
+          ? "bg-gray-600 hover:bg-gray-700 px-3"
+          : "bg-green-600 hover:bg-green-700 px-5"
+      }`}
+    >
+      {connectAccounts ? "Connected" : "Conect"}
+    </button>
+  );
+}
+
+export default ButtonConnect;
