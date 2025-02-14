83f6b1e0a8b3d874e9dcdcc459d3193770208432
faeze <faezeh.am4212@gmail.com>
2025-02-14 19:24:29 +0330

    Changeُtructure

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,26 +1,21 @@
 // import React from 'react'
-import Sidebar from './Sidebar'
-import Header from './Header'
-import React from 'react'
+import Sidebar from "./Sidebar";
+import Header from "./Header";
+import React from "react";
 import { useStore } from "../store";
 
 const Layout = (props: { children: React.ReactNode }) => {
+  const menuBar = useStore((state) => state.menuBar);
 
-    const menuBar = useStore((state) => state.menuBar);
+  return (
+    <div className="flex flex-row bg-gray-900 text-gray-50 overflow-hidden">
+      <Sidebar />
+      <div className={`header overflow-y-auto h-screen w-screen`}>
+        <Header />
+        {props.children}
+      </div>
+    </div>
+  );
+};
 
-    return (
-        <div className="grid grid-cols-12 bg-gray-900 text-gray-50 ">
-            <Sidebar />
-            <div className={`header  ${menuBar ? " col-span-11" : " col-span-10"}`}>
-                <Header />
-                {props.children}
-            </div>
-
-        
-
-
-        </div>
-    )
-}
-
-export default Layout
+export default Layout;
