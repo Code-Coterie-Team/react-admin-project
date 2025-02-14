e274b7a145184e566011b7ec92aa0042dfdb47be
faeze <faezeh.am4212@gmail.com>
2025-02-14 19:23:06 +0330

    edit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-    
-        "grayb": "rgb(55,65,81)",
-        "bgHover":"rgb(55,65,81)",
-        "menuBody":"rgb(31,41,55)",
-        "bmenuBody":"rgb(55,65,81)",
+        grayb: "rgb(55,65,81)",
+        bgHover: "rgb(55,65,81)",
+        menuBody: "rgb(31,41,55)",
+        bmenuBody: "rgb(55,65,81)",
       },
-      boxShadow:{
-      //  "itemMenuOverview":" rgba(0, 0, 0, 0) 0px 0px 0px 0px, rgba(0, 0, 0, 0) 0px 0px 0px 0px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.1) 0px 4px 6px -4px",
-       "itemMenuOverview":" rgba(0, 0, 0, 0) 0px 0px 0px 0px, rgba(0, 0, 0, 0) 0px 0px 0px 0px, rgba(0, 0, 0, 0.1) 0px 29px 15px 15px, rgba(0, 0, 0, 0.1) 0px 4px 6px -4px",
+      boxShadow: {
+        itemMenuOverview:
+          " rgba(0, 0, 0, 0) 0px 0px 0px 0px, rgba(0, 0, 0, 0) 0px 0px 0px 0px, rgba(0, 0, 0, 0.1) 0px 29px 15px 15px, rgba(0, 0, 0, 0.1) 0px 4px 6px -4px",
       },
     },
   },
   plugins: [],
-}
-
+};
