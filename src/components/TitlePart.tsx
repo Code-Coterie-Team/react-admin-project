177a6cdd49ca7aa26ed3202ce726636a01b9bfff
faeze <faezeh.am4212@gmail.com>
2025-02-14 12:29:45 +0330

    createTitlePart

---
+++ src/components/TitlePart.tsx
@@ -0,0 +1,12 @@
+import { ITitlePart } from "../types/api";
+
+function TitlePart(props: ITitlePart) {
+  return (
+    <div className="flex flex-row gap-4 mb-4">
+      {props.iconTitlePart}
+      <h2 className="font-semibold text-xl text-gray-100">{props.titlePart}</h2>
+    </div>
+  );
+}
+
+export default TitlePart;
