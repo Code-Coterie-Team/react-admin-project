ee86ae09d9e3ed210bb8faba427571e3caf6ccf1
faeze <faezeh.am4212@gmail.com>
2025-02-14 12:29:28 +0330

    useTitlePartAndUseToggleButton

--- src/components/Security.tsx
+++ src/components/Security.tsx
@@ -2,23 +2,25 @@ import React, { useState } from "react";
 import { Lock01 } from "../icons";
 import ButtonSetting from "./ButtonSetting";
 import ToggleButton from "./ToggleButton";
+import TitlePart from "./TitlePart";
 
 function Security() {
   return (
     <div className="security mb-8 bg-menuBody p-6 rounded-lg border-bmenuBody border border-solid">
-      <div className="flex flex-row gap-4 mb-4">
-        <Lock01
-          className="text-indigo-400"
-          width="1.75rem"
-          height="1.75rem"
-        />
-        <h2 className="text-xl font-semibold text-gray-100">Security</h2>
-      </div>
-
+      <TitlePart
+        iconTitlePart={
+          <Lock01
+            className="text-indigo-400"
+            width="1.75rem"
+            height="1.75rem"
+          />
+        }
+        titlePart={"Security"}
+      />
       <div className="togggleButtonSecurity text-base">
         <div className=" flex justify-between items-center py-3">
           <span className="text-gray-300 ">Two-Factor Authentication</span>
-          <ToggleButton initialState={false}/>
+          <ToggleButton initialState={false} />
         </div>
       </div>
       <div className="mt-4">
