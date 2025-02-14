70e20bff1a76c8b540d49d2baa21423578876e5f
faeze <faezeh.am4212@gmail.com>
2025-02-14 12:27:39 +0330

    useTitlePart

--- src/components/Notifications.tsx
+++ src/components/Notifications.tsx
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
 import { Notification03 } from "../icons";
 import ToggleButton from "./ToggleButton";
 import ItemNOtifiication from "./ItemNOtifiication";
+import TitlePart from "./TitlePart";
 
 function Notifications() {
   const listNotifications = [
@@ -20,14 +20,16 @@ function Notifications() {
   ];
   return (
     <div className="notifications mb-8 bg-menuBody p-6 rounded-lg border-bmenuBody border border-solid">
-      <div className="flex flex-row gap-4 mb-4">
-        <Notification03
-          className="text-indigo-400"
-          width="1.75rem"
-          height="1.75rem"
-        />
-        <h2 className="text-xl font-semibold text-gray-100">Notifications</h2>
-      </div>
+      <ItemConnectAccount
+        iconTitlePart={
+          <Notification03
+            className="text-indigo-400"
+            width="1.75rem"
+            height="1.75rem"
+          />
+        }
+        titlePart={"Notifications"}
+      />
       <ItemNOtifiication itemNotification={listNotifications} />
     </div>
   );
