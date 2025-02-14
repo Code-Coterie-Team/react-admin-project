5b6aea9baf2972a43c78db8b071453327d9ee1b8
faeze <faezeh.am4212@gmail.com>
2025-02-14 19:25:42 +0330

    deleteState

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,10 +2,8 @@
 import Sidebar from "./Sidebar";
 import Header from "./Header";
 import React from "react";
-import { useStore } from "../store";
 
 const Layout = (props: { children: React.ReactNode }) => {
-  const menuBar = useStore((state) => state.menuBar);
 
   return (
     <div className="flex flex-row bg-gray-900 text-gray-50 overflow-hidden">
